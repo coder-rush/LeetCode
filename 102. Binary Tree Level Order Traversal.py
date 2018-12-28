b73fccb858a04f2070531f11c67b599ce804817b
coder-rush <rushilsharma@ymail.com>
2018-12-27 22:53:34 -0500

    Hard, DFS , Cache

---
+++ 102. Binary Tree Level Order Traversal.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if not root:
+            return []
+        ans = []
+        level = [root]
+        #Initialize level 1 with root
+        while level:
+            #Append all the nodes at this level to the answer
+            ans.append([node.val for node in level])
+            next_level = []
+            #Calculate nodes at next level, i.e for each node at this level at add next level
+            for node in level:
+                next_level.append(node.left)
+                next_level.append(node.right)
+            level = []
+            for node in next_level:
+                if node:
+                    level.append(node)
+        return ans
