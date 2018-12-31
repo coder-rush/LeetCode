bc1da6216f19e7b0865d53a216c03bb882929b6c
coder-rush <rushilsharma@ymail.com>
2018-12-31 17:09:05 -0500

    Easy, DP

---
+++ 107. Binary Tree Level Order Traversal II.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def levelOrderBottom(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if not root:
+            return []
+        level = [root]
+        ans = []
+        while len(level) != 0:
+            ans.insert(0, [node.val for node in level])
+            next_level = []
+            for node in level:
+                if node.left:
+                    next_level.append(node.left)
+                if node.right:
+                    next_level.append(node.right)
+            level = [node for node in next_level if node]
+
+        return ans
