3819dfc5e9cfd8d3252ab9f8b3728d08955da544
coder-rush <rushilsharma@ymail.com>
2019-01-13 10:01:20 -0500

    Good, bucket

---
+++ 75. Sort Colors.py
@@ -0,0 +1,18 @@
+class Solution:
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        Beautiful O(n) solition        """
+        i = 0
+        j = 0
+        k = 0
+        for k in range(len(nums)):
+            n = nums[k]
+            nums[k] = 2
+            if n<2:
+                nums[j] = 1
+                j+=1
+            if n==0:
+                nums[i] = 0
+                i+= 1
